4432019192efcb2a5f821b5b7f6e5832a7998c47
ryand0ng <91922917+ryand0ng@users.noreply.github.com>
2024-12-05 01:04:11 -0500

    F

--- app.js
+++ app.js
@@ -8,14 +8,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
-let collection;
-(async() => {
-    const client = new MongoClient(uri);
-    await client.connect();
-    collection = client.db(process.env.MONGO_DB_NAME).collection(process.env.MONGO_COLLECTION);
-})();
 
 app.get('/', async (req, res) => {
+    const client = new MongoClient(uri);
+    await client.connect();
+    let collection = client.db(process.env.MONGO_DB_NAME).collection(process.env.MONGO_COLLECTION);
     const response = await fetch(
         `https://api.nasa.gov/planetary/apod?api_key=${process.env.API_KEY}`
     );
@@ -25,6 +22,9 @@ app.get('/', async (req, res) => {
 });
 
 app.post('/comment', async (req, res) => {
+    const client = new MongoClient(uri);
+    await client.connect();
+    let collection = client.db(process.env.MONGO_DB_NAME).collection(process.env.MONGO_COLLECTION);
     const text = req.body.text;
     const comment = { text: text, date: new Date() };
     await collection.insertOne(comment);
